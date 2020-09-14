d7088c3500d6e13faf0f9b1961cf463e5b8708fe
DoKiNet <info@dokinet.com>
2020-09-14 11:03:32 +0200

    Build 1.1.16

--- include/create_persistent_partition.sh
+++ include/create_persistent_partition.sh
@@ -4,7 +4,20 @@ set -e
 
 AMIAN_PARTITION=$(df | grep "/run/live/medium" | cut -d  ' ' -f 1)
 DEVICE=${AMIAN_PARTITION:0:8}
+PERSISTENT_PARTITION="$DEVICE"2
 
 f1(){
-	lsblk|grep sdb2 #DEVICE2
+	lsblk -p |grep $PERSISTENT_PARTITION
 }
+
+if  [ "$(f1)" ]; then
+    echo " "$PERSISTENT_PARTITION FOUND
+	mount $PERSISTENT_PARTITION /AMIAN_DATA
+    echo " "$PERSISTENT_PARTITION MOUNTED IN /AMIAN_DATA
+else
+	echo " "$PERSISTENT_PARTITION NOT FOUND
+    #creo la partizione sfruttando tutto lo spazio non allocato
+    #/dev/sdb2
+    #mkfs.ext4 -L AMIAN_DATA /dev/sdb2
+	#mount DEVICE2 /AMIAN_DATA
+fi
