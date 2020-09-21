e40d2b7bd6f134bf9b3ed50c8d4f88e398d22625
DoKiNet <info@dokinet.com>
2020-09-21 17:55:29 +0200

    Build 1.2.2

--- include/start_gvm.sh
+++ include/start_gvm.sh
@@ -3,25 +3,44 @@
 set -e
 
 check_persistent_partition(){
-    #to implement
-    #find DEVICE2 mounted in /AMIAN_DATA
+    AMIAN_PARTITION=$(df | grep "/run/live/medium" | cut -d  ' ' -f 1)
+    DEVICE=${AMIAN_PARTITION:0:8}
+    PERSISTENT_PARTITION="$DEVICE"2
+    if [ "$(df --output=source,target)" == "$PERSISTENT_PARTITION      /AMIAN_DATA" ]; then
+        echo "1"
+    else
+        echo "0"
+    fi
 }
 
-if [ "$(check_persistent_partition)" ]; then
+sudo su
+
+if [ "$(check_persistent_partition)" == "1" ]; then
     echo ""
-    echo " PERSISTENT PARTITION: FOUND"
-else
+    echo "PERSISTENT PARTITION: FOUND"
+elif [ "$(check_persistent_partition)" == "0" ]; then
     echo ""
-    echo " PERSISTENT PARTITION: NOT FOUND"
-    echo " "
-    echo " I'M CREATING A PERSISTENT PARTITION ON UNALLOCATED SPACE..."
+    echo "PERSISTENT PARTITION: NOT FOUND"
+    echo ""
+    echo "I'M CREATING A PERSISTENT PARTITION ON UNALLOCATED SPACE..."
     /root/create_persistent_partition.sh
 fi
 
-#if /AMIAN_DATA/Dbox/GVM11 found
-#         dbox start gvm11
-#         lxc-attach .. gvmi start gvm11
-#else cp gvm11.dbox /AMIAN_DATA/Dbox/
-#         tar -xvzf
-#         dbox start gvm11
-#         lxc-attach .. gvmi start gvm11
+if [ -d "/AMIAN_DATA/DBox/gvm11" ]; then
+    echo ""
+    echo "Directory \"/AMIAN_DATA/DBox/gvm11\": FOUND"
+    dbox start gvm11
+    lxc-attach -n gvm11 -- gvmi start gvm11
+else
+    cp /root/gvm11.dbox /AMIAN_DATA/Dbox/
+    cd /AMIAN_DATA/Dbox/
+    tar -xvzf gvm11.dbox
+    rm gvm11.dbox
+    dbox start gvm11
+    lxc-attach -n gvm11 -- gvmi start gvm11
+fi
+
+
+#get gvm11 ip address
+#firefox https://ipaddress:9392
+#import ssl certificate in firefox
